6b2db31f2f46301f270cb758b7daca2a2d5eb066
long-yong <long.yong@outlook.com>
2018-07-27 21:49:31 -0700

    change pwd

--- mysql-01.py
+++ mysql-01.py
@@ -4,7 +4,7 @@
 import pymysql
 
 # db = pymysql.connect("localhost", "root", "", "TEST")
-db = pymysql.connect(host='127.0.0.1', port=3306, user='root', passwd='', db='TEST', charset='utf8')
+db = pymysql.connect(host='127.0.0.1', port=3306, user='root', passwd='root', db='TEST', charset='utf8')
 
 cursor = db.cursor()
 cursor.execute("SELECT VERSION()")
--- mysql-02.py
+++ mysql-02.py
@@ -4,7 +4,7 @@
 import pymysql
 
 # db = pymysql.connect("localhost", "root", "", "TEST")
-db = pymysql.connect(host='127.0.0.1', port=3306, user='root', passwd='', db='test', charset='utf8')
+db = pymysql.connect(host='127.0.0.1', port=3306, user='root', passwd='root', db='test', charset='utf8')
 
 cursor = db.cursor()
 cursor.execute("SELECT VERSION()")
